201a3374aa468e1a3ab5fb05103febc5dbdef8a9
Male Michael <malmike21@outlook.com>
2016-12-22 21:01:57 +0300

    The constants file contains application constants that are to be used within the application

---
+++ app/src/main/java/com/example/malmike21/orahiapp/constants/Constants.java
@@ -0,0 +1,41 @@
+package com.example.malmike21.orahiapp.constants;
+
+/**
+ * Created by malmike21 on 15/12/2016.
+ */
+
+public class Constants {
+    //service constants
+    public static String HOTELS="hotels";
+    public static String EVENTS="events";
+    public static String RESTAURANTS="restaurants";
+    public static String HOUSES="houses";
+    public static String CABS="cabs";
+    public static String BODAS="bodas";
+    public static String DELIVERIES="deliveries";
+    public static String EXCHANGES="exchanges";
+    public static String BANKS="banks";
+    public static String MMONEY="mmoney";
+    public static String POLICE="police";
+    public static String WEKUME="wekume";
+
+    //URL constant
+    public static String GOOGLE_MAPS_URL = "https://maps.googleapis.com/";
+
+    public static final String BASE_URL = "http://orahi.cloudapp.net/";
+    public static final String REGISTER_OPERATION = "register";
+    public static final String LOGIN_OPERATION = "login";
+    public static final String CHANGE_PASSWORD_OPERATION = "chgPass";
+
+    public static final Boolean SUCCESS = true;
+    public static final String FAILURE = "failure";
+    public static final String IS_LOGGED_IN = "isLoggedIn";
+
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String UNIQUE_ID = "token";
+
+    public static final String TAG = "Orahi";
+
+}
